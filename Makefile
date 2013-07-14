5e57a7be3b0f75e43f2da6fdb189b1740fb04e35
Colin Patrick Mccabe <cmccabe@alumni.cmu.edu>
2013-07-14 16:09:12 -0700

    reproduce bug more often
    Signed-off-by: Colin McCabe <cmccabe@alumni.cmu.edu>

--- Makefile
+++ Makefile
@@ -1,11 +1,12 @@
 CC=g++
-CFLAGS=-Wall -Wextra
-BOOST_INSTALL_DIR=/opt/boost-trunk
+CFLAGS=-g -O0 -Wall -Wextra #-std=c++0x
+#BOOST_INSTALL_DIR=/opt/boost_1_54_0/
+BOOST_INSTALL_DIR=/usr
 
 boost-bug-test: boost-bug-test.cc
 	$(CC) $(CFLAGS) boost-bug-test.cc -lboost_system -lboost_thread -lpthread \
-		-L$(BOOST_INSTALL_DIR)/lib -I$(BOOST_INSTALL_DIR)/include \
-		-o boost-bug-test
+		-L$(BOOST_INSTALL_DIR)/lib64 -L$(BOOST_INSTALL_DIR)/lib \
+		-I$(BOOST_INSTALL_DIR)/include -o boost-bug-test
 
 run: boost-bug-test
 	LD_LIBRARY_PATH="$(LD_LIBRARY_PATH):$(BOOST_INSTALL_DIR)/lib" ./boost-bug-test
--- boost-bug-test.cc
+++ boost-bug-test.cc
@@ -1,27 +1,37 @@
+#include <boost/shared_ptr.hpp>
 #include <boost/thread.hpp>
 #include <boost/thread/condition_variable.hpp>
-#include <memory>
+#include <tr1/memory>
 #include <stdio.h>
 
-using std::auto_ptr;
+#define NTHREADS 10
 
 boost::mutex *g_mutex;
 
 boost::condition_variable *g_cvar;
 
+typedef std::vector<std::tr1::shared_ptr<boost::thread> > thread_vec_t;
+
 void run_thread() {
   printf("run_thread...\n");
-  boost::mutex::scoped_lock lock(*g_mutex);
-  g_cvar->wait(lock);
-  printf("thread exiting.\n");
+  while (true) {
+    boost::mutex::scoped_lock lock(*g_mutex);
+    g_cvar->wait(lock);
+  }
 }
 
 int main() {
+  thread_vec_t threads;
   g_mutex = new boost::mutex();
   g_cvar = new boost::condition_variable();
-  printf("starting thread...\n");
-  auto_ptr<boost::thread> thread(new boost::thread(run_thread));
-  boost::mutex::scoped_lock lock(*g_mutex);
-  g_cvar->notify_one();
+  for (int i = 0; i < NTHREADS; i++) {
+    printf("starting thread...\n");
+    threads.push_back(std::tr1::shared_ptr<boost::thread>(
+        new boost::thread(run_thread)));
+  }
+  while (true) {
+    boost::mutex::scoped_lock lock(*g_mutex);
+    g_cvar->notify_one();
+  }
   return 0;
 }
